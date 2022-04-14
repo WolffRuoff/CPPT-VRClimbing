e3e4aa1452270a8090eba05e9b4683d98c377b8a
WolffRuoff <ethan@ruoff.us>
2022-04-13 20:50:25 -0400

    Added climb mechanic

--- Assets/Scripts/ClimbTransformer.cs
+++ Assets/Scripts/ClimbTransformer.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
+/************************************************************************************
+Copyright : Copyright (c) Facebook Technologies, LLC and its affiliates. All rights reserved.
+
+Your use of this SDK or tool is subject to the Oculus SDK License Agreement, available at
+https://developer.oculus.com/licenses/oculussdk/
+
+Unless required by applicable law or agreed to in writing, the Utilities SDK distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ANY KIND, either express or implied. See the License for the specific language governing
+permissions and limitations under the License.
+************************************************************************************/
+
 using UnityEngine;
 
-public class ClimbTransformer : MonoBehaviour
+namespace Oculus.Interaction
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// A Transformer that moves the oculus camera in a 1-1 fashion inverted with the GrabPoint.
+    /// Updates transform the target in such a way as to maintain the target's
+    /// local positional and rotational offsets from the GrabPoint.
+    /// </summary>
+    public class ClimbTransformer : MonoBehaviour, ITransformer
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private IGrabbable _grabbable;
+        private Pose _previousGrabPose;
+
+        public void Initialize(IGrabbable grabbable)
+        {
+            _grabbable = grabbable;
+        }
+
+        public void BeginTransform()
+        {
+            Pose grabPoint = _grabbable.GrabPoints[0];
+            _previousGrabPose = grabPoint;
+        }
+
+        public void UpdateTransform()
+        {
+            Pose grabPoint = _grabbable.GrabPoints[0];
+            var targetTransform = _grabbable.Transform;
+
+            Vector3 worldOffsetFromGrab = targetTransform.position - _previousGrabPose.position;
+            Vector3 offsetInGrabSpace = Quaternion.Inverse(_previousGrabPose.rotation) * worldOffsetFromGrab;
+            //Quaternion rotationInGrabSpace = Quaternion.Inverse(_previousGrabPose.rotation) * targetTransform.rotation;
+
+            targetTransform.position = (grabPoint.rotation * offsetInGrabSpace) + grabPoint.position;
+            //targetTransform.rotation = grabPoint.rotation * rotationInGrabSpace;
+
+            _previousGrabPose = grabPoint;
+        }
+
+        public void EndTransform() { }
     }
 }
